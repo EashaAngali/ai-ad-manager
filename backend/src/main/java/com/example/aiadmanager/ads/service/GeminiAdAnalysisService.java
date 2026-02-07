479bafdafc1ce0c5d1ad8aa901e7a8afd65a6934
EashaAngali <eashaangalikhatri@gmail.com>
2026-02-07 22:36:32 +0500

    Update API URL to use stable model version

--- backend/src/main/java/com/example/aiadmanager/ads/service/GeminiAdAnalysisService.java
+++ backend/src/main/java/com/example/aiadmanager/ads/service/GeminiAdAnalysisService.java
@@ -70,8 +70,9 @@ public class GeminiAdAnalysisService {
                 )
         );
 
-        String url = "https://generativelanguage.googleapis.com/v1beta/models/"
-                + model + ":generateContent?key=" + apiKey;
+       String url = "https://generativelanguage.googleapis.com/v1/models/"
+        + model + ":generateContent?key=" + apiKey;
+
 
         String aiRaw;
         try {
